b24746e657fd16624c2caaf620acd17139cf089a
Ashwin Gopalsamy <ashwin.gopalsamy.extern@solarisbank.de>
2024-07-07 07:04:14 +0530

    include helpers for tests

--- go-fundamentals/01-hello/hello_you_test.go
+++ go-fundamentals/01-hello/hello_you_test.go
@@ -6,17 +6,20 @@ func TestHelloYou(t *testing.T) {
 	t.Run("Saying Hello to People", func(t *testing.T) {
 		got := HelloName("Ashwin")
 		want := "Hello, Ashwin! 👋"
-		if got != want {
-			t.Errorf("want: %q, got: %q", want, got)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying Hello to World", func(t *testing.T) {
 		got := HelloName("")
 		want := "Hello, World! 👋"
-		if got != want {
-			t.Errorf("want: %q, got:%q", want, got)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("want: %q, got:%q", want, got)
+	}
 }
